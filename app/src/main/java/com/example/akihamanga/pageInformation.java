8e32f1ca5b49a51f2a9109f9a427b0601e4275f8
Melione <cazenavemathilde@gmail.com>
2022-03-19 16:23:04 +0100

    Interfaces

---
+++ app/src/main/java/com/example/akihamanga/pageInformation.java
@@ -0,0 +1,14 @@
+package com.example.akihamanga;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pageInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_information);
+    }
+}
