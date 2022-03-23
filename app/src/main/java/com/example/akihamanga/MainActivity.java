edc16a40ee9837e488a413017d05963e189b868f
Melione <cazenavemathilde@gmail.com>
2022-03-23 19:21:45 +0100

    Ajout page de connexion - popup

--- app/src/main/java/com/example/akihamanga/MainActivity.java
+++ app/src/main/java/com/example/akihamanga/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.akihamanga;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button b_seconnecter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b_seconnecter = (Button) findViewById(R.id.b_seconnecter);
+        b_seconnecter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openConnexion();
+            }
+        });
     }
+
+    public void openConnexion()
+    {}
 }
