22a8366dca02262b81735a906449a376b28f16dc
TrixJoestar <tristan.goncalves25@gmail.com>
2022-03-23 23:17:40 +0100

    Page détail du volume créé (Brut)

---
+++ app/src/main/java/com/example/akihamanga/detailsVolume.java
@@ -0,0 +1,14 @@
+package com.example.akihamanga;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class detailsVolume extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_volume);
+    }
+}
