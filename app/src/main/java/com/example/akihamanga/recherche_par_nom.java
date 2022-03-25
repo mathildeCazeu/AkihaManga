e75534af6010b816f2eb4eac38debf6718c3434d
TrixJoestar <tristan.goncalves25@gmail.com>
2022-03-26 00:59:59 +0100

    Page de connexion (liste de favoris)

---
+++ app/src/main/java/com/example/akihamanga/recherche_par_nom.java
@@ -0,0 +1,14 @@
+package com.example.akihamanga;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class recherche_par_nom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recherche_par_nom);
+    }
+}
