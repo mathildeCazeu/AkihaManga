57b2f67c79779cc6bfdf636fe915f5d0a9507023
TrixJoestar <tristan.goncalves25@gmail.com>
2022-03-25 23:02:13 +0100

    Finition de page de favoris et création de la page de recherche en ligne

---
+++ app/src/main/java/com/example/akihamanga/recherche_en_ligne.java
@@ -0,0 +1,14 @@
+package com.example.akihamanga;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class recherche_en_ligne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recherche_en_ligne);
+    }
+}
