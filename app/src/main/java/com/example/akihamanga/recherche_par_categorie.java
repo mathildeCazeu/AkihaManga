dc147046e35da7ab61086cf89d743a573ef81419
TrixJoestar <tristan.goncalves25@gmail.com>
2022-03-26 11:29:57 +0100

    Création de la page "recherche_par_categorie"

---
+++ app/src/main/java/com/example/akihamanga/recherche_par_categorie.java
@@ -0,0 +1,14 @@
+package com.example.akihamanga;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class recherche_par_categorie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recherche_par_categorie);
+    }
+}
